596367defa63c17642e17a941a1504259a91c7f1
PeterMule <19330314+PeterMule@users.noreply.github.com>
2023-04-26 00:08:35 +0300

    Create 1-insertion_sort_list.c

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,60 @@
+#include "sort.h"
+
+void swap_list(listint_t *pn, listint_t *temp, listint_t *nxt, listint_t **h);
+/**
+ * insertion_sort_list - sorts list with insertion sort
+ * @list: input list to sort
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp, *pn, *nxt;
+
+	if (list && *list && (*list)->next)
+	{
+		temp = (*list)->next;
+		pn = temp->prev;
+		nxt = temp->next;
+
+		while (temp)
+		{
+			if (temp->n < pn->n)
+			{
+				swap_list(pn, temp, nxt, list);
+			}
+			temp = nxt;
+			if (temp)
+			{
+				nxt = temp->next;
+				pn = temp->prev;
+			}
+		}
+	}
+}
+
+/**
+ * swap_list - swaps the position of two nodes in a linked list
+ * @pn: previous node
+ * @temp: temp
+ * @nxt: next node
+ * @h: head of list
+ */
+void swap_list(listint_t *pn, listint_t *temp, listint_t *nxt, listint_t **h)
+{
+
+	while (pn && temp->n < pn->n)
+	{
+		if (nxt)
+			nxt->prev = pn;
+		temp->next = pn;
+		temp->prev = pn->prev;
+		if (pn->prev)
+			(pn->prev)->next = temp;
+		pn->prev = temp;
+		pn->next = nxt;
+		nxt = pn;
+		pn = temp->prev;
+		if ((*h)->prev)
+			*h = (*h)->prev;
+		print_list(*h);
+	}
+}
