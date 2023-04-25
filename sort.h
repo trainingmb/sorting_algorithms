8ce7549ea3bb82ffdf4e208a238b8c817268359d
PeterMule <19330314+PeterMule@users.noreply.github.com>
2023-04-25 23:41:35 +0300

    Update sort.h

--- sort.h
+++ sort.h
@@ -21,7 +21,9 @@ typedef struct listint_s
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 
-//Sorting Algorithms
+/**
+ * Sorting Algorithms
+ */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
