36df9e865229f5cbc890bf8747401b4b619bbc8f
PeterMule <19330314+PeterMule@users.noreply.github.com>
2023-04-25 23:34:27 +0300

    Create 0-bubble_sort.c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array of integers
+ * using Bubble Sort
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void bubble_sort(int *array, size_t size)
+{
+    size_t i;
+    char changed;
+
+    i = 1;
+    changed = 'a';
+    while(changed =='a')
+    {
+        changed = 'b';
+        while (array && i < size)
+        {
+            if (array[i] < array[i - 1])
+            {  
+                array[i]     = array[i] ^ array[i - 1];
+                array[i - 1] = array[i] ^ array[i - 1];
+                array[i]     = array[i] ^ array[i - 1];
+                changed = 'a';
+            }
+        }
+        print_array(array, size);
+    }
+}
